403290911d1e973d6af170a52cfa5f68d676014c
Adnane Dinar <100191048+F0rday@users.noreply.github.com>
2023-02-13 15:48:46 +0100

    First commit

---
+++ server.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+class Robot:
+	def __init__(self,name):
+		print("creating robot")
+		self.name = name
+
+	def forward(self,speed):
+		self.speed = speed
+		print('avance')
+
+robot = Robot()
+@app.route("/")
+def getPage():
+	templateData = {
+		'title' : 'Robot Control'
+	}
+	return render_template('index.html', **templateData)
+	
+@app.route("/forward", methods=['GET', 'POST'])
+def forward():
+	# robot_travel(True)
+	return ('', 204)
+
+@app.route("/backward", methods=['GET', 'POST'])
+def backward():
+	# robot_travel(False)
+	return ('', 204)
+
+@app.route("/turnRight", methods=['GET', 'POST'])
+def turn_right():
+	# robot_turn(True)
+	return ('', 204)
+
+@app.route("/turnLeft", methods=['GET', 'POST'])
+def turn_left():
+	# robot_turn(False)
+	return ('', 204)
+
+@app.route("/stop", methods=['GET', 'POST'])
+def stop():
+	# robot_stop()
+	return ('', 204)
+
+try:
+	if __name__ == "__main__":
+		# setup_GPIO()
+		app.run(host='0.0.0.0', port=5002, debug=True)
+finally:
+	# GPIO.cleanup()
+	print("finish")
